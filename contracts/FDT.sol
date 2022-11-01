4d692b6786dc66498f2392ae1afae3bda6ba74e6
장정훈 <jangjeonghun@jangjeonghun-ui-iMac.local>
2022-11-01 18:03:24 +0900

    feat: add user info

--- contracts/FDT.sol
+++ contracts/FDT.sol
@@ -5,12 +5,25 @@ import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "./lib/IERC5192.sol";
 
 contract FDT is Initializable, ERC721URIStorageUpgradeable, IERC5192, OwnableUpgradeable, UUPSUpgradeable {
+    using SafeMathUpgradeable for uint256;
     using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    struct User {
+        string id;
+        address userAddress;
+        string class;
+        string name;
+    }
+
     CountersUpgradeable.Counter private _tokenIds;
-    mapping(uint256 => bool) private lock;
+    mapping(uint256 => bool) private _lock;
+    mapping(uint256 => User) private _user;
+    mapping(string => uint256) private _voting;
+    mapping(string => uint256) private _nextVoting;
 
     function initialize() initializer public {
         __ERC721_init("Figlio De Taejon", "FDT");
@@ -18,26 +31,79 @@ contract FDT is Initializable, ERC721URIStorageUpgradeable, IERC5192, OwnableUpg
         __Ownable_init();
     }
 
-    function mintNFTWithLock(address recipient, string memory tokenURI) public {
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    /*
+        Mint & Burn
+     */
+    function mintWithLock(address recipient, string calldata tokenURI, User calldata userInfo) public {
         uint256 id = _tokenIds.current();
+        setUser(id, userInfo);
         _mint(recipient, id);
         _setTokenURI(id, tokenURI);
-        lock[id] = true;
-        emit Locked(id);
+        lock(id);
         _tokenIds.increment();
     }
-    
-    function locked(uint256 tokenId) public view returns (bool) {
-        return lock[tokenId];
-    }
 
-    function burn(uint256 tokenId) public onlyOwner {
+    function burn(uint256 tokenId) public {
+        require((_msgSender() == ownerOf(tokenId)) || (_msgSender() == owner()),
+            "Caller is not the owner or has not the token");
+        delete _lock[tokenId];
         _burn(tokenId);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override {
-        require(locked(tokenId) == false, "token is locked");
+    /*
+        User
+    */
+    function setUser(uint256 tokenId, User calldata userInfo) public onlyOwner {
+        require(address(0) == _user[tokenId].userAddress, "User already existed");
+        _user[tokenId] = userInfo;
+        addVoting(tokenId);
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+    function classAdvancement(uint256 tokenId, string calldata class) public onlyOwner {
+        _user[tokenId].class = class;
+    }
+
+    /*
+        Voting
+     */
+    function addVoting(uint256 tokenId) internal onlyOwner {
+        string memory userId = _user[tokenId].id;
+        _voting[userId] = _voting[userId].add(1);
+    }
+
+    function addNextVoting(uint256 tokenId) internal onlyOwner {
+        string memory userId = _user[tokenId].id;
+        _nextVoting[userId] = _nextVoting[userId].add(1);
+    }
+
+    function changeToNextVoting() external onlyOwner {
+        uint256 currentTokenId = _tokenIds.current();
+        for(uint256 i = 0; i < currentTokenId ;i++) {
+            string memory userId = _user[i].id;
+            _voting[userId] = _nextVoting[userId];
+        }
+    }
+    
+    /*
+        Lock
+     */
+    function locked(uint256 tokenId) public view override returns (bool) {
+        return _lock[tokenId];
+    }
+
+    function lock(uint256 tokenId) public onlyOwner {
+        _lock[tokenId] = true;
+        emit Locked(tokenId);
+    }
+
+    function unlock(uint256 tokenId) public onlyOwner {
+        _lock[tokenId] = false;
+        emit Unlocked(tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override {
+        require(locked(tokenId) != true, "token is locked");
+    }
 }
